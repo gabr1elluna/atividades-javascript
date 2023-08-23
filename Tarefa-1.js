4fb850fef7c25df2b5e39731d32644b58b6f4103
Gabriel Luna <sougabrielluna@gmail.com>
2023-08-23 08:58:56 -0300

    Tarefa-1.js

--- Tarefa-1.js
+++ Tarefa-1.js
@@ -1,15 +1,2 @@
  var nome = prompt("Insira seu nome ao lado: ")
-console.log('im here strocking my dick mr'+ nome);
-/* var idade
- var estadoCivil
-
-
-
-
-
-if(estadoCivil == 2){
-    console.log('Solteiro')
-}else if(estadoCivil == 1){
-    console.log('Casado')
-}
-console.log(`Nome: ${nome}\nIdade: ${idade}\nEstado Civil: ${estadoCivil}`)*/
+console.log('Hello Mr/Ms '+ nome);
